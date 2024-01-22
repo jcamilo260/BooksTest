2ab9e0eb5e266899b8a080eae858fa963fb353dc
Juan Camilo Arguelles Ardila <juancamiloarguellesardila@gmail.com>
2024-01-22 01:15:39 -0500

    This is the beginning, there's just an empty project here

---
+++ Timetonics Book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timetonics Book
+//
+//  Created by Juan Camilo Argüelles Ardila on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timetonics Book/Timetonics_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timetonics_BookApp.swift
+//  Timetonics Book
+//
+//  Created by Juan Camilo Argüelles Ardila on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Timetonics_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
